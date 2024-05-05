16fff092161d4c1ecbf5f722d3e7c512deb92813
lolifamily <lolifamilies@gmail.com>
2024-05-05 18:12:53 +0800

    Create lolifamily.is-an.app.js

---
+++ domains/lolifamily.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Lolifamily\'s personal blog',
+  domain: 'is-an.app',
+  subdomain: 'lolifamily',
+  owner: {
+    email: 'lolifamilies@gmail.com',
+  },
+  record: {
+    NS: ['ns1.huaweicloud-dns.com', 'ns1.huaweicloud-dns.cn', 'ns1.huaweicloud-dns.net', 'ns1.huaweicloud-dns.org'],
+  },
+  proxy: false,
+})
